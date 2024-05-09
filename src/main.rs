21c92ed2e933b6e722d7058ee5fa10f8acb19056
Gendiro <grabberisonfire@gmail.com>
2024-05-09 20:15:04 +0300

    REST API skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,63 @@
 #[macro_use] extern crate rocket;
 
-use std::ops::Deref;
-use std::sync::{Arc, Mutex};
-use rocket::serde::{Deserialize, json::Json};
-use rocket::State;
+use diesel::sql_types::Uuid;
+use rocket::serde::{Serialize, Deserialize, json::Json};
 
-struct MyState {
-    names: Arc<Mutex<Vec<String>>>
+#[derive(Serialize, Deserialize)]
+struct GetUserIdResponse {
+   user_id: Uuid
 }
 
-#[get("/")]
-fn index(state: &State<MyState>) -> String {
-    let mut result_string = String::new();
-    let names_mutex = state.names.lock().unwrap();
-    let names = names_mutex.deref();
-    for name in names {
-        result_string.push_str(name.as_str());
-    }
-    result_string
+#[derive(Serialize, Deserialize)]
+struct GetTopicsResponse {
+    topics: Vec<String>
 }
 
-#[derive(Deserialize)]
-#[serde(crate = "rocket::serde")]
-struct User {
-    name: String,
-    age: i32
+#[derive(Serialize, Deserialize)]
+struct Message {
+    user_id: Uuid,
+    context: String
 }
 
-#[post("/", format="json", data = "<user>")]
-fn post_index(user: Json<User>, state: &State<MyState>) {
-    let mut names = state.names.lock().unwrap();
-    names.push(user.name.clone());
+#[derive(Serialize, Deserialize)]
+struct GetMessagesResponse {
+    messages: Vec<Message>
+}
+
+#[derive(Serialize, Deserialize)]
+struct PostMessageRequest {
+    user_id: Uuid,
+    topic_id: String,
+    message: String
+}
+
+#[get("/get_user_id")]
+fn get_user_id() -> Json<GetUserIdResponse> {
+
+}
+
+#[get("/topics")]
+fn get_topics() -> Json<GetTopicsResponse> {
+
+}
+
+#[get("/get_messages")]
+fn get_messages() -> Json<GetMessagesResponse> {
+
+}
+
+#[post("/write_message", format = "json", data = "<request>")]
+fn post_message(request: Json<PostMessageRequest>) {
+
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, post_index])
-        .manage(MyState { names: Arc::new(Mutex::new(Vec::new()))})
+        .mount("/", routes![
+            get_user_id,
+            get_topics,
+            get_messages,
+            post_message
+        ])
 }
